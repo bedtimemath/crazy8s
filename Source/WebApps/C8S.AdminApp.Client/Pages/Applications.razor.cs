0792a1a691dbbdbdcd591b4ede1a59e98a6f7671
dugsteen <dug@softcrow.net>
2024-11-13 06:33:14 -0700

    Better sidebar and filter layout

--- Source/WebApps/C8S.AdminApp.Client/Pages/Applications.razor.cs
+++ Source/WebApps/C8S.AdminApp.Client/Pages/Applications.razor.cs
@@ -9,21 +9,37 @@ namespace C8S.AdminApp.Client.Pages;
 
 public partial class Applications: BaseRazorPage
 {
+    private readonly IList<SortDropDownOption> _sortDropDownOptions = [
+        new( "Submitted (newest)", "SubmittedOn DESC" ),
+        new( "Submitted (oldest)", "SubmittedOn ASC" ),
+        new( "Last Name (A-Z)", "ApplicantLastName ASC" ),
+        new( "Last Name (Z-A)", "ApplicantLastName ASC" ),
+        new( "Email (A-Z)", "ApplicantEmail ASC" ),
+        new( "Email (Z-A)", "ApplicantEmail DESC" )
+    ];
+
     [Inject]
     public ILogger<Applications> Logger { get; set; } = default!;
 
     private ApplicationsLister _applicationsLister = default!;
-    private string _sortDescription = "ApplicantEmail DESC";
+    private RadzenDropDown<IList<ApplicationStatus>> _statusDropDown = default!;
+    private RadzenDropDown<string> _sortDropDown = default!;
+
+    private string _selectedSort = "SubmittedOn DESC";
+    private IList<ApplicationStatus> _selectedStatuses = [ApplicationStatus.Received];
+
     private int? _totalCount;
 
-    private IList<ApplicationStatus> _selectedStatuses = 
-        new [] { ApplicationStatus.Received };
-    private RadzenDropDown<IList<ApplicationStatus>> _statusDropDown = default!;
+    private async Task HandleSortDropdownChange(object args)
+    {
 
+    }
     private async Task HandleStatusDropdownChange(object args)
     {
         var statuses = args as EnumerableQuery<ApplicationStatus> ??
                        throw new UnreachableException();
 
     }
+
+    private record SortDropDownOption(string Display, string Value);
 }
