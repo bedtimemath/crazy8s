acaeacab4931710e23e747d653b83864e1bc60ff
dugsteen <dug@softcrow.net>
2025-01-20 15:21:47 -0700

    Adding the request call section

--- Source/Domains/C8S.Domain.Features/Requests/Models/RequestDetails.cs
+++ Source/Domains/C8S.Domain.Features/Requests/Models/RequestDetails.cs
@@ -1,5 +1,12 @@
-﻿namespace C8S.Domain.Features.Requests.Models;
+﻿using C8S.Domain.Enums;
+
+namespace C8S.Domain.Features.Requests.Models;
 
 public record RequestDetails: RequestAbstract
 {
+    public ApplicantType? PersonType { get; init; }
+    public string? PersonPhone { get; init; }
+    public string? ReferenceSource { get; init; }
+    public string? ReferenceSourceOther { get; init; }
+    public string? Comments { get; init; }
 }
---
+++ Source/WebApps/C8S.AdminApp.Client.UI/Requests/RequestCallSection.razor.cs
@@ -0,0 +1,33 @@
+﻿using C8S.AdminApp.Client.Services.Coordinators.Requests;
+using C8S.AdminApp.Client.UI.Base;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Logging;
+using SC.Common.Extensions;
+
+namespace C8S.AdminApp.Client.UI.Requests;
+
+public sealed partial class RequestCallSection: BaseClientComponent
+{
+    [Inject]
+    public ILogger<RequestCallSection> Logger { get; set; } = null!;
+    
+    [Parameter]
+    public RequestDetailsCoordinator Coordinator { get; set; } = null!;
+
+    protected override void OnInitialized()
+    {
+        Coordinator.DetailsUpdated += HandleDetailsUpdated;
+        base.OnInitialized();
+    }
+
+    private void LogPhone(string input, bool good) =>
+        Logger.LogDebug("{Result}: {Input} => {Number}", good ? "GOOD" : "BAD", input,  input.DisplayPhone());
+
+    public void Dispose()
+    {
+        Coordinator.DetailsUpdated -= HandleDetailsUpdated;
+    }
+
+    private void HandleDetailsUpdated(object? sender, EventArgs e) => 
+        StateHasChanged();
+}
